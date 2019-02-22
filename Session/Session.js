b4e60f43c33daaa972e98b6f3f1d4c77df84cf2c
miguel <mroncancio19@gmail.com>
2019-02-22 17:05:16 -0500

    included capabilties object within Session class

--- Session/Session.js
+++ Session/Session.js
@@ -3,9 +3,10 @@ const uuidv1 = require('uuid/v1');
 const Browser = require('../browser/browser.js');
 
 class Session {
-  constructor() {
+  constructor(capabilities) {
     this.id = uuidv1(); // creates RFC4122 (IEFT) UUID according to W3C standard
     this.browser = new Browser(); // TODO create default config file, pass to constructor
+    this.capabilities = capabilities;
   }
 }
 
