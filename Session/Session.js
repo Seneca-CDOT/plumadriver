83538bb5aae572e08747b7d6138e4c484c53f016
miguel <mroncancio19@gmail.com>
2019-02-27 10:24:20 -0500

    begin firstmatch capabilties implementation

--- Session/Session.js
+++ Session/Session.js
@@ -1,43 +1,71 @@
 
 const uuidv1 = require('uuid/v1');
 const Browser = require('../browser/browser.js');
-const { defaultRequiredCapabilities } = require('./Capabilities/defaultCapabilties');
 const { BadRequest } = require('../Error/errors');
 const { Capability } = require('./Capability/Capability');
 
 class Session {
   constructor(requestedCapabilities) {
+    this.processCapabilities(requestedCapabilities);
     this.id = uuidv1(); // creates RFC4122 (IEFT) UUID according to W3C standard
     this.browser = new Browser(); // TODO create default config file, pass to constructor
     this.capabilities = {
       requiredCapabilities: [],
       firstMatchCapabilties: [],
     };
-    this.processCapabilities(requestedCapabilities);
   }
 
-  processCapabilities(requestedCapabilities) {
+  processCapabilities(request) {
+    const defaultCapabilities = [
+      'acceptInsecureCerts',
+      'browserName',
+      'browserVersion',
+      'platformName',
+      'pageLoadStrategy',
+    ];
+
     let capabilities;
     const capabiltiesRequest = Object.prototype.hasOwnProperty
-      .call(requestedCapabilities, 'capabilities');
+      .call(request, 'capabilities');
     if (!capabiltiesRequest) {
       throw new BadRequest('invalid argument');
     } else {
-      capabilities = requestedCapabilities.capabilities;
+      capabilities = request.capabilities;
     }
 
-    if (capabilities.alwaysMatch !== undefined ) {
-      Object.keys(defaultRequiredCapabilities).forEach((key) => {
+    // alwaysMatch capabilties
+    const requiredCapabilities = {};
+    if (capabilities.alwaysMatch !== undefined) {
+      Object.keys(defaultCapabilities).forEach((key) => {
         if (Object.prototype.hasOwnProperty.call(capabilities.alwaysMatch, key)) {
-          try {
-            const capability = new Capability(capabilities.alwaysMatch[key], key);
-            this.capabilities.requiredCapabilities.push(capability);
-          } catch (error) {
-            throw error;
-          }
+          const validatedCapability = new Capability(capabilities.alwaysMatch[key], key);
+          requiredCapabilities[key] = validatedCapability;
         }
       });
     }
+
+    // first match capabilities
+    let allMatchedCapabilities = capabilities.firstMatch;
+    if (allMatchedCapabilities === undefined) {
+      allMatchedCapabilities = [{}];
+    } else if (allMatchedCapabilities.constructor.name.toLowerCase() !== 'array' || allMatchedCapabilities.length === 0) {
+      throw new BadRequest('invalid argument');
+    }
+
+    const validatedFirstMatchCapabilties = [];
+    
+    allMatchedCapabilities.forEach((capabilityName) => {
+      try {
+        const validatedCapability = new Capability(); // TODO: add parameters.
+
+      } catch (error) {
+        throw error;
+      }
+    });
+
+
+
+    return this; // change, here so eslint is not so annoying
   }
 }
 
