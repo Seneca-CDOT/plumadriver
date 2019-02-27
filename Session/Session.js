773256eaea689f708124f06765979ad246513693
miguel <mroncancio19@gmail.com>
2019-02-27 13:32:00 -0500

    start merging capabilities

--- Session/Session.js
+++ Session/Session.js
@@ -1,12 +1,14 @@
 
 const uuidv1 = require('uuid/v1');
 const Browser = require('../browser/browser.js');
-const { BadRequest } = require('../Error/errors');
+const { BadRequest, InternalServerError } = require('../Error/errors');
 const { Capability } = require('./Capability/Capability');
 
 class Session {
   constructor(requestedCapabilities) {
-    this.processCapabilities(requestedCapabilities);
+    if (Session.processCapabilities(requestedCapabilities) === null) {
+      throw new InternalServerError('session not created');
+    }
     this.id = uuidv1(); // creates RFC4122 (IEFT) UUID according to W3C standard
     this.browser = new Browser(); // TODO create default config file, pass to constructor
     this.capabilities = {
@@ -15,7 +17,7 @@ class Session {
     };
   }
 
-  processCapabilities(request) {
+  static processCapabilities(request) {
     const defaultCapabilities = [
       'acceptInsecureCerts',
       'browserName',
@@ -48,24 +50,36 @@ class Session {
     let allMatchedCapabilities = capabilities.firstMatch;
     if (allMatchedCapabilities === undefined) {
       allMatchedCapabilities = [{}];
-    } else if (allMatchedCapabilities.constructor.name.toLowerCase() !== 'array' || allMatchedCapabilities.length === 0) {
+    } else if (allMatchedCapabilities.constructor.name.toLowerCase() !== 'array'
+      || allMatchedCapabilities.length === 0) {
       throw new BadRequest('invalid argument');
     }
 
     const validatedFirstMatchCapabilties = [];
-    
-    allMatchedCapabilities.forEach((capabilityName) => {
-      try {
-        const validatedCapability = new Capability(); // TODO: add parameters.
 
-      } catch (error) {
-        throw error;
-      }
+    allMatchedCapabilities.forEach((capabilityName) => {
+      const validatedCapability = new Capability(
+        allMatchedCapabilities[capabilityName],
+        capabilityName,
+      );
+      validatedFirstMatchCapabilties.push(validatedCapability);
     });
 
+    // attempt mergin capabilities
+    const mergedCapabilities = Capability.mergeCapabilities(requiredCapabilities, firstMatchCapabilities);
+  }
+
+  static mergeCapabilities(primary, secondary) {
+    const result = {};
+
+    Object.keys(primary).forEach((key) => {
+      result[key] = primary[key];
+    });
+
+    if (secondary === undefined) return result;
 
 
-    return this; // change, here so eslint is not so annoying
+    return result;
   }
 }
 
