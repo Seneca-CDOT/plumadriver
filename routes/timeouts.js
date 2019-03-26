ee7c2009698609093370c6472663ebea21a3ea69
miguel <mroncancio19@gmail.com>
2019-03-26 17:13:17 -0400

    removed async from endpoint, not needed

--- routes/timeouts.js
+++ routes/timeouts.js
@@ -6,7 +6,7 @@ timeouts.get('/', (req, res, next) => {
 });
 
 // set timeouts
-timeouts.post('/', async (req, res, next) => {
+timeouts.post('/', (req, res, next) => {
   req.session.setTimeouts(req.body);
   res.send(null);
 });
