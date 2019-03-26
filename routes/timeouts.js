49ec41aa85f7ac8fcb3c71fcd04a12166e53ac8c
miguel <mroncancio19@gmail.com>
2019-03-26 12:09:40 -0400

    implemented setTimeouts endpoint

--- routes/timeouts.js
+++ routes/timeouts.js
@@ -1,11 +1,14 @@
 const timeouts = require('express').Router();
+const { InvalidArgument } = require('../Error/errors');
 
 timeouts.get('/', (req, res, next) => {
-    
+
 });
 
-timeouts.post('/', (req, res, next) => {
-
+// set timeouts
+timeouts.post('/', async (req, res, next) => {
+  req.session.setTimeouts(req.body);
+  res.send(null);
 });
 
 module.exports = timeouts;
