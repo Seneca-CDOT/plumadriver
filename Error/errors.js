67cf9983f3e687fdc7b236f161c7ccb17388c5ed
miguel <mroncancio19@gmail.com>
2019-03-27 17:19:05 -0400

    added NotFoundError to exports

--- Error/errors.js
+++ Error/errors.js
@@ -1,5 +1,6 @@
 module.exports.MethodNotAllowed = require('./MethodNotAllowedError'); // remove
 module.exports.NoSuchElement = require('./NoSuchElement');
+module.exports.NotFoundError = require('./NotFoundError');
 module.exports.RequestTimeout = require('./RequestTimeoutError'); // remove
 module.exports.InvalidArgument = require('./InvalidArgument');
 module.exports.SessionNotCreated = require('./SessionNotCreated');
