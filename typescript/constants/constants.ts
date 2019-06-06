abaf154fb04c0cc03afda91d54ff1999de7e2f39
Miguel Roncancio <mroncanicio19@gmail.com>
2019-06-05 20:32:26 -0400

    add runScriptValues and COMMANDS constants

--- typescript/constants/constants.ts
+++ typescript/constants/constants.ts
@@ -1,7 +1,6 @@
 import { StringUnion } from '../utils/utils';
 
-// Allowed Element boolean attribute values 
-export const ElementBooleanAttribute = StringUnion(
+export const ElementBooleanAttributeValues = StringUnion(
    'async'
   , 'autocomplete'
   , 'autofocus'
@@ -41,5 +40,37 @@ export const ElementBooleanAttribute = StringUnion(
   , 'translate'
 );
 
+export const UnhandledPromptBehaviourValues = StringUnion(
+  'accept'
+  , 'dismiss'
+  , 'dismiss and notify'
+  , 'accept and notify'
+  , 'ignore'
+);
+
+export const RunScriptsValues = StringUnion(
+  'dangerously', 'outside-only', ''
+);
+
 // webelement identifier
 export const ELEMENT = 'element-6066-11e4-a52e-4f735466cecf';
+
+export const COMMANDS = {
+  DELETE_SESSION: 'DELETE SESSION',
+  GET_TIMEOUTS: 'GET TIMEOUTS',
+  SET_TIMEOUTS: 'SET TIMEOUTS',
+  NAVIGATE_TO: 'NAVIGATE TO',
+  GET_TITLE: 'GET TITLE',
+  FIND_ELEMENT: 'FIND ELEMENT',
+  FIND_ELEMENTS: 'FIND ELEMENTS',
+  FIND_ELEMENT_FROM_ELEMENT: ' FIND ELEMENT FROM ELEMENT',
+  FIND_ELEMENTS_FROM_ELEMENT: 'FIND ELEMENTS FROM ELEMENTS',
+  GET_ELEMENT_TEXT: 'GET ELEMENT TEXT',
+  GET_CURRENT_URL: 'GET CURRENT URL',
+  GET_ALL_COOKIES: 'GET ALL COOKIES',
+  ADD_COOKIE: 'ADD COOKIE',
+  GET_ELEMENT_TAG_NAME: 'GET ELEMENT TAG NAME',
+  GET_ELEMENT_ATTRIBUTE: 'GET ELEMENT ATTRIBUTE',
+  EXECUTE_SCRIPT: 'EXECUTE SCRIPT',
+  ELEMENT_SEND_KEYS: 'ELEMENT SEND KEYS',
+};
