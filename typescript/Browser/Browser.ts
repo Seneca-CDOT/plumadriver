118c49e28b20da94f1fc389a136daa6d17afe0cd
miguel <mroncancio19@gmail.com>
2019-07-08 16:39:35 -0400

    merge browser to ts

--- typescript/Browser/Browser.ts
+++ typescript/Browser/Browser.ts
@@ -1,37 +1,36 @@
-import { toughCookie, JSDOM, ResourceLoader } from 'jsdom';
+import { JSDOM, ResourceLoader } from 'jsdom';
 import { BrowserConfig } from './BrowserConfig';
 import { Pluma } from '../Types/types';
 import { ELEMENT } from '../constants/constants';
 import { WebElement } from '../WebElement/WebElement';
-import  * as Utils  from '../utils/utils';
+import * as Utils from '../utils/utils';
 
-const { Cookie } = toughCookie;
+import { tough } from '../../jsdom_extensions/tough-cookie';
 
 // TODO: include Error classes after migratring to typescript
-
 
 /**
  * Plumadriver browser with jsdom at its core.
  * @param {BrowserConfig} browserConfig contains the user-defined jsdom configuration for the session
  * @param {Array<WebElement>} knownElements the list of known elements https://www.w3.org/TR/webdriver1/#elements
  * @param {JSDOM} dom jsdom object
- * @param {HTMLElement} activeElement the browser's active element 
+ * @param {HTMLElement} activeElement the browser's active element
  */
 export class Browser {
   browserConfig: BrowserConfig;
   knownElements: Array<WebElement>;
-  dom:JSDOM;
-  activeElement:HTMLElement | null;
+  dom: JSDOM;
+  activeElement: HTMLElement | null;
 
   constructor(capabilities: object) {
-    
     let browserOptions: Pluma.BrowserOptions = {
       runScripts: '',
       strictSSL: true,
       unhandledPromptBehaviour: 'dismiss and notify',
+      rejectPublicSuffixes: false
     };
 
-    Object.keys(browserOptions).forEach((option) => {
+    Object.keys(browserOptions).forEach(option => {
       if (capabilities[option]) browserOptions[option] = capabilities[option];
     });
 
@@ -40,26 +39,43 @@ export class Browser {
 
   /**
    * Creates an empty jsdom object or from a url depending on if the url parameter was passed
-   * @param config @type {BrowserConfig} the jsdom configuration for the session 
-   * @param url @type {URL} an optional url 
+   * @param config @type {BrowserConfig} the browser configuration for a given session
+   * @param url @type {URL} an optional url
    */
-  async configureBrowser(config:BrowserConfig, url:URL) {
+  async configureBrowser(
+    config: BrowserConfig,
+    url: URL,
+    pathType: string = 'url'
+  ) {
     let dom;
 
     if (url !== null) {
-      dom = await JSDOM.fromURL(url, {
-        resources: config.resourceLoader,
-        runScripts: config.runScripts,
-        beforeParse: config.beforeParse,
-      });
+      if (pathType === 'url') {
+        dom = await JSDOM.fromURL(url, {
+          resources: config.resourceLoader,
+          runScripts: config.runScripts,
+          beforeParse: config.beforeParse,
+          pretendToBeVisual: true,
+          cookieJar: config.jar
+        });
+      } else if (pathType === 'file') {
+        dom = await JSDOM.fromFile(url, {
+          resources: config.resourceLoader,
+          runScripts: config.runScripts,
+          beforeParse: config.beforeParse,
+          pretendToBeVisual: true,
+          cookieJar: config.jar
+        });
+      }
 
       /*  promise resolves after load event has fired. Allows onload events to execute
       before the DOM object can be manipulated  */
-      const loadEvent = () => new Promise((resolve) => {
-        dom.window.addEventListener('load', () => {
-          resolve(dom);
+      const loadEvent = () =>
+        new Promise(resolve => {
+          dom.window.addEventListener('load', () => {
+            resolve(dom);
+          });
         });
-      });
 
       this.dom = await loadEvent();
     } else {
@@ -67,6 +83,8 @@ export class Browser {
         resources: config.resourceLoader,
         runScripts: config.runScripts,
         beforeParse: config.beforeParse,
+        pretendToBeVisual: true,
+        cookieJar: config.jar
       });
     }
 
@@ -75,78 +93,69 @@ export class Browser {
     this.activeElement = this.dom.window.document.activeElement;
   }
 
-  async navigateToURL(url:URL) {
+  async navigateToURL(url: URL, pathType) {
     if (url) {
-      await this.configureBrowser(this.browserConfig, url);
+      await this.configureBrowser(this.browserConfig, url, pathType);
     }
+    return true;
   }
 
   /**
    * Returns the current page title
    */
-  get title() {return this.dom.window.document.title;}
+  getTitle() {
+    return this.dom.window.document.title;
+  }
 
-/**
- * returns the current page url
- */
-  get url() {return this.dom.window.document.URL;}
+  /**
+   * returns the current page url
+   */
+  getUrl() {
+    return this.dom.window.document.URL;
+  }
 
   /**
    * sets a cookie on the browser
    */
-  set cookie(cookie) {
-    
-    if (Utils.isCookie(cookie)) {
-      const scheme = this.url().substr(0, this.url().indexOf(':'));
-      if (scheme !== 'http' && scheme !== 'https' && scheme !== 'ftp') throw new InvalidArgument();
+  addCookie(cookie) {
+    if (cookie === null || cookie === undefined) throw new InvalidArgument();
 
-      // validates cookie
-      Object.keys(Utils.validateCookie).forEach((key) => {
-        if (Object.prototype.hasOwnProperty.call(cookie, key)) {
-          if (key === 'domain') {
-            if (!Utils.validateCookie[key](cookie[key], this.url())) {
-              throw new InvalidArgument();
-            }
-          } else if (!Utils.validateCookie[key](cookie[key])) {
-            throw new InvalidArgument();
-          }
-        }
+    const scheme = this.getUrl().substr(0, this.getUrl().indexOf(':'));
+
+    if (scheme !== 'http' && scheme !== 'https' && scheme !== 'ftp')
+      throw new InvalidArgument();
+
+    if (Utils.isValidCookie(cookie, this.getUrl())) {
+      let validCookie;
+
+      Object.keys(cookie).forEach(key => {
+        if (key === 'name') validCookie.key = cookie[key];
+        else if (key === 'expiry') validCookie.expires = cookie[key];
+        else validCookie[key] = cookie[key];
       });
 
-    } else {
-      throw new InvalidArgument();
-    }
-
-    //------------------------------------------------------------------------------------------
-
-    const validCookie:any = {};
-
-    // stores validated cookie properties in validCookie
-    Object.keys(cookie).forEach((key) => {
-      if (key === 'name') validCookie.key = cookie[key];
-      else if (key === 'expiry') validCookie.expires = cookie[key];
-      else validCookie[key] = cookie[key];
-    });
-
-    // create tough cookie and store in jsdom cookie jar
-    try {
-      this.dom.cookieJar.store.putCookie(new Cookie(validCookie), err => err);
-    } catch (err) {
-      throw new Error('UNABLE TO SET COOKIE'); // need to create this error class
-    }
+      try {
+        this.dom.cookieJar.store.putCookie(
+          new tough.Cookie(validCookie),
+          err => err
+        );
+      } catch (err) {
+        throw new Error('UNABLE TO SET COOKIE'); // need to create this error class
+      }
+    } else throw new InvalidArgument('');
   }
 
   /**
    * returns all cookies in the cookie jar
    */
-  get cookies() {
+  getCookies() {
     const cookies = [];
 
     this.dom.cookieJar.serialize((err, serializedJar) => {
       if (err) throw err;
-      serializedJar.cookies.forEach((cookie) => {
-        const currentCookie:any = {};
-        Object.keys(cookie).forEach((key) => {
+      serializedJar.cookies.forEach(cookie => {
+        const currentCookie: any = {};
+        Object.keys(cookie).forEach(key => {
           // renames 'key' property to 'name' for W3C compliance and selenium functionality
           if (key === 'key') currentCookie.name = cookie[key];
           else if (key === 'expires') {
@@ -164,12 +173,11 @@ export class Browser {
   }
 
   /**
-   * 
    * @param elementId @type {string} the id of a known element in the known element list
    */
-  getKnownElement(elementId:string):WebElement {
+  getKnownElement(elementId: string): WebElement {
     let foundElement = null;
-    this.knownElements.forEach((element) => {
+    this.knownElements.forEach(element => {
       if (element[ELEMENT] === elementId) foundElement = element;
     });
     if (!foundElement) throw new NoSuchElement();
--- typescript/Browser/BrowserConfig.ts
+++ typescript/Browser/BrowserConfig.ts
@@ -1,5 +1,6 @@
 import { ResourceLoader } from 'jsdom';
 import { Pluma } from '../Types/types';
+import { tough } from '../../jsdom_extensions/tough-cookie';
 
 import * as Utils from '../utils/utils';
 
@@ -8,25 +9,40 @@ import * as Utils from '../utils/utils';
  */
 export class BrowserConfig {
   readonly runScripts: Pluma.RunScripts = '';
-
-  readonly strictSSL: Boolean = true;
-
+  readonly strictSSL: boolean = true;
   readonly unhandledPromptBehaviour: Pluma.UnhandledPromptBehaviour = 'dismiss';
-
   readonly resourceLoader: ResourceLoader;
-
   readonly beforeParse: Pluma.BeforeParse;
+  readonly jar;
+  readonly rejectPublicSuffixes : boolean;
 
   constructor(options: Pluma.BrowserOptions) {
-    this.resourceLoader = new ResourceLoader({
-      strictSSL: this.strictSSL,
-    });
-
     if (!Utils.isBrowserOptions(options))
       throw new Error('Invalid jsdom options');
 
-    Object.keys(options).forEach((option) => {
+    Object.keys(options).forEach(option => {
+      if (option === 'strictSSL' && typeof options[option] !== 'boolean')
+        throw new InvalidArgument('');
+
+      if (
+        option === 'rejectPublicSuffixes' &&
+        typeof options[option] !== 'boolean'
+      )
+        throw new InvalidArgument('');
+
       this[option] = options[option];
+    });
+
+    this.resourceLoader = new ResourceLoader({
+      strictSSL: this.strictSSL
+    });
+
+    this.jar = new tough.CookieJar(new tough.MemoryCookieStore(), {
+      looseMode: true,
+      rejectPublicSuffixes:
+        typeof options.rejectPublicSuffixes === 'boolean'
+          ? options.rejectPublicSuffixes
+          : true
     });
 
     switch (options.unhandledPromptBehaviour) {
@@ -37,13 +53,13 @@ export class BrowserConfig {
         this.beforeParse = BrowserConfig.beforeParseFactory(() => false);
         break;
       case 'dismiss and notify':
-        this.beforeParse = BrowserConfig.beforeParseFactory((message) => {
+        this.beforeParse = BrowserConfig.beforeParseFactory(message => {
           console.log(message);
           return false;
         });
         break;
       case 'accept and notify':
-        this.beforeParse = BrowserConfig.beforeParseFactory((message) => {
+        this.beforeParse = BrowserConfig.beforeParseFactory(message => {
           console.log(message);
           return true;
         });
@@ -55,9 +71,9 @@ export class BrowserConfig {
     }
   }
 
-  static beforeParseFactory(func: Pluma.BeforeParse) {
-    return (window) => {
-      ['confirm', 'alert', 'prompt'].forEach((method) => {
+  static beforeParseFactory(func: Pluma.UserPrompt) {
+    return window => {
+      ['confirm', 'alert', 'prompt'].forEach(method => {
         window[method] = func;
       });
     };
