97dab53aac5e2b2d38589759ab33c8e43b729543
miguel <mroncancio19@gmail.com>
2019-07-08 18:00:30 -0400

    add validate object

--- typescript/utils/utils.ts
+++ typescript/utils/utils.ts
@@ -136,3 +136,27 @@ export const isBrowserOptions = (obj: any): obj is Pluma.BrowserOptions => {
     return false;
   return true;
 };
+
+export const validate = {
+  requestBodyType(incomingMessage, type) {
+    if (incomingMessage.headers['content-type'].includes(type)) {
+      return true;
+    }
+    return false;
+  },
+
+  objectPropertiesAreInArray(object, array) {
+    let validObject = true;
+
+    Object.keys(object).forEach((key) => {
+      if (!array.includes(key)) validObject = false;
+    });
+
+    if (validObject && Object.keys(object).length > array.length) validObject = false;
+
+    return validObject;
+  },
+  isEmpty(obj) {
+    return (Object.keys(obj).length === 0);
+  },
+}
