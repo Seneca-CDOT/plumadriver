c63739550333f73b983a729af3f3ef02f90e0de2
miguel <mroncancio19@gmail.com>
2019-07-09 15:52:03 -0400

    merge endpoints to ts

---
+++ typescript/routes/timeouts.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import { InvalidArgument } from '../Error/errors';
+import { COMMANDS } from '../constants/constants';
+
+const timeouts = express.Router
+
+timeouts.get('/', (req, res, next) => {
+  const response = { value: req.session.getTimeouts() };
+  res.json(response);
+});
+
+// set timeouts
+timeouts.post('/', (req, res, next) => {
+  req.session.setTimeouts(req.body);
+  res.send(null);
+});
+
+export default timeouts; 
