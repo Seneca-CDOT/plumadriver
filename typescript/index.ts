5ad7ac642d7738b0a734f944b527066ba511302f
miguel <mroncancio19@gmail.com>
2019-07-09 15:58:53 -0400

    merge main server entry point to ts

---
+++ typescript/index.ts
@@ -0,0 +1,102 @@
+
+import * as express from 'express';
+import argv from 'minimist'; // for user provided port
+import bodyParser from 'body-parser';
+import * as  winston from 'winston';
+import expressWinston from 'express-winston';
+
+import { SessionManager } from './SessionManager/SessionManager';
+import { InvalidArgument } from './Error/errors.js';
+import  router  from './routes/index';
+
+const args = argv((process.argv.slice(2)));
+const server = express();
+const HTTP_PORT = process.env.PORT || args.port || 3000;
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+const sessionsManager = new SessionManager();
+
+server.set('sessionsManager', sessionsManager);
+
+// middleware
+
+server.use(bodyParser.json());
+
+// request logging
+if (process.env.NODE_ENV !== 'test') {
+  const reqTransports = process.env.NODE_ENV === 'test'
+    ? [
+      new winston.transports.Console({ level: 'info' }),
+      new winston.transports.File({ filename: 'pluma_requests.txt', level: 'info' }),
+    ]
+    : [new winston.transports.File({ filename: 'pluma_requests.txt', level: 'info' })];
+
+
+  server.use(expressWinston.logger({
+    transports: reqTransports,
+    format: winston.format.combine(
+      winston.format.json(),
+      winston.format.prettyPrint(),
+    ),
+    meta: true,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    expressFormat: true,
+    colorize: false,
+  }));
+}
+
+
+async function onHTTPStart() {
+  console.log(`listening on port ${HTTP_PORT}`);
+}
+
+/* -------- ENDPOINTS -------------------- */
+
+server.get('/', (req, res) => {
+  res.redirect(/* insert rick roll link here */);
+});
+
+// Status
+server.get('/status', (req, res) => {
+  const state = sessionsManager.getReadinessState();
+  res.status(200).json(state);
+});
+
+
+/*---------------------------------------------------------*/
+
+server.use(router);
+
+if (process.env.NODE_ENV !== 'test') {
+  // error logging
+  const errTransports = process.env.NODE_ENV === 'test'
+    ? [
+      new winston.transports.Console({ level: 'error'}),
+      new winston.transports.File({ filename: 'pluma_error_log.txt', level: 'error' }),
+    ]
+    : [new winston.transports.File({ filename: 'pluma_error_log.txt', level: 'error' })];
+  server.use(expressWinston.errorLogger({
+    transports: errTransports,
+    format: winston.format.combine(
+      winston.format.json(),
+      winston.format.prettyPrint(),
+    ),
+  }));
+}
+
+
+// error handler
+// eslint-disable-next-line no-unused-vars
+server.use((err, req, res, next) => {
+  let error;
+  if (err instanceof SyntaxError) error = new InvalidArgument('Syntax Error');
+  if (error === undefined) res.status(500).json(err);
+  else res.status(error.code).json(error);
+  process.exit(0);
+});
+
+server.listen(HTTP_PORT, async () => {
+  await onHTTPStart();
+});
+
+module.exports = server; // for testing
