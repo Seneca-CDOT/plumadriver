c9746d53395804c61c764ff1a8895c40c10a4abe
miguel <mroncancio19@gmail.com>
2019-07-18 12:46:24 -0400

    clean up error logging and error response

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-
 import * as express from 'express';
-import * as  argv from 'minimist'; // for user provided port
+import * as argv from 'minimist'; // for user provided port
 import * as bodyParser from 'body-parser';
-import * as  winston from 'winston';
+import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
+import { Pluma } from './Types/types';
 
 import { SessionManager } from './SessionManager/SessionManager';
-import { InvalidArgument } from './Error/errors';
-import router  from './routes/index';
+import router from './routes/index';
 
 const args = argv(process.argv.slice(2));
 const server = express();
@@ -24,75 +23,99 @@ server.use(bodyParser.json());
 
 // request logging
 if (process.env.NODE_ENV !== 'test') {
-  const reqTransports = process.env.NODE_ENV === 'test'
-    ? [
-      new winston.transports.Console({ level: 'info' }),
-      new winston.transports.File({ filename: 'pluma_requests.txt', level: 'info' }),
-    ]
-    : [new winston.transports.File({ filename: 'pluma_requests.txt', level: 'info' })];
+  const reqTransports =
+    process.env.NODE_ENV === 'test'
+      ? [
+          new winston.transports.Console({ level: 'info' }),
+          new winston.transports.File({
+            filename: 'pluma_requests.txt',
+            level: 'info',
+          }),
+        ]
+      : [
+          new winston.transports.File({
+            filename: 'pluma_requests.txt',
+            level: 'info',
+          }),
+        ];
 
-
-  server.use(expressWinston.logger({
-    transports: reqTransports,
-    format: winston.format.combine(
-      winston.format.json(),
-      winston.format.prettyPrint(),
-    ),
-    meta: true,
-    msg: 'HTTP {{req.method}} {{req.url}}',
-    expressFormat: true,
-    colorize: false,
-  }));
+  server.use(
+    expressWinston.logger({
+      transports: reqTransports,
+      format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+      ),
+      meta: true,
+      msg: 'HTTP {{req.method}} {{req.url}}',
+      expressFormat: true,
+      colorize: false,
+    }),
+  );
 }
-
 
 async function onHTTPStart() {
-  console.log(`listening on port ${HTTP_PORT}`);
+  console.log(`plumadriver listening on port ${HTTP_PORT}`);
 }
 
-/* -------- ENDPOINTS -------------------- */
-
-// Status
 router.get('/status', (req, res) => {
   const state = sessionManager.getReadinessState();
   res.status(200).json(state);
 });
 
-
-/*---------------------------------------------------------*/
-
 server.use('/', router);
 
-if (process.env.NODE_ENV !== 'test') {
-  // error logging
-  const errTransports = process.env.NODE_ENV === 'test'
+// error logging
+const errTransports =
+  process.env.NODE_ENV === 'test'
     ? [
-      new winston.transports.Console({ level: 'error'}),
-      new winston.transports.File({ filename: 'pluma_error_log.txt', level: 'error' }),
-    ]
-    : [new winston.transports.File({ filename: 'pluma_error_log.txt', level: 'error' })];
-  server.use(expressWinston.errorLogger({
+        new winston.transports.Console({ level: 'error' }),
+        new winston.transports.File({
+          filename: 'pluma_error_log.txt',
+          level: 'error',
+          handleExceptions: true,
+        }),
+      ]
+    : [
+        new winston.transports.File({
+          filename: 'pluma_error_log.txt',
+          level: 'error',
+          handleExceptions: true,
+        }),
+      ];
+
+server.use(
+  expressWinston.errorLogger({
     transports: errTransports,
     format: winston.format.combine(
-      winston.format.json(),
-      winston.format.prettyPrint(),
+      winston.format.timestamp(),
+      winston.format.printf((err) => {
+        return `[PLUMA ERROR]
+        \n${err.meta.date}
+        \n${err.meta.stack}        
+        \n\treq: ${err.meta.req.method} ${err.meta.req.url}
+-------------------------------------------------------------------------------------
+        `;
+      }),
     ),
-  }));
-}
-
+  }),
+);
 
 // error handler
 // eslint-disable-next-line no-unused-vars
 server.use((err, req, res, next) => {
-  let error;
-  if (err instanceof SyntaxError) error = new InvalidArgument('Syntax Error');
-  if (error === undefined) res.status(500).json(err);
-  else res.status(error.code).json(error);
-  process.exit(0);
+  let errorResponse: Pluma.ErrorResponse = {
+    value: {
+      error: err.JSONCodeError,
+      message: err.message,
+      stacktrace: err.stack,
+    },
+  };
+  res.status(err.code).json(errorResponse);
 });
 
 server.listen(HTTP_PORT, async () => {
   await onHTTPStart();
 });
 
-module.exports = server; // for testing
+ export {server};
