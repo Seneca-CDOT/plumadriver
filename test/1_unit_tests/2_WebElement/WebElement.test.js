bbe078cb33884bbf46784abaffda56ee8c764948
Pouya Oftadeh <poftadeh2@gmail.com>
2019-09-16 18:36:06 -0400

    feat: add radio button click test

--- test/1_unit_tests/2_WebElement/WebElement.test.js
+++ test/1_unit_tests/2_WebElement/WebElement.test.js
@@ -1,1 +1,24 @@
 const { JSDOM } = require('jsdom');
+const { WebElement } = require('../../../build/WebElement/WebElement');
+const PAGES = {
+  RADIOS: './pages/radios.html',
+};
+const path = require('path');
+
+const generateDom = async page => {
+  const dom = await JSDOM.fromFile(path.join(__dirname, page));
+  return dom;
+};
+
+describe('Radio Elements', () => {
+  it('checks a radio button', async () => {
+    const {
+      window: { document },
+    } = await generateDom(PAGES.RADIOS);
+
+    const radioButton = document.querySelector('input[type="radio"]');
+    const radioElement = new WebElement(radioButton);
+    radioElement.click();
+    expect(radioButton.checked).toBe(true);
+  });
+});
