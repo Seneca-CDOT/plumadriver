bf807b3ae54a7a15db15f1fafbd048befe8cb211
Pouya Oftadeh <poftadeh2@gmail.com>
2019-09-25 17:34:54 -0400

    feat: create InvalidElementState error

---
+++ src/Error/InvalidElementState.ts
@@ -0,0 +1,11 @@
+import { BadRequest } from './BadRequest';
+
+export class InvalidElementState extends BadRequest {
+  constructor() {
+    super();
+    this.message =
+      'A command could not be completed because the element is in an invalid state';
+    this.name = 'InvalidElementStateError';
+    this.JSONCodeError = 'invalid element state';
+  }
+}
