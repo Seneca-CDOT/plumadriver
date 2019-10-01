cfbc86af49c6782ef014ba4033af88569a9b3641
Pouya Oftadeh <poftadeh2@gmail.com>
2019-10-01 14:02:23 -0400

    refactor: remove legacy folder

--- legacy/Error/InternalServerError.js
+++
@@ -1,9 +0,0 @@
-const WebDriverError = require('./WebDriverError');
-
-class InternalServerError extends WebDriverError {
-  constructor(message) {
-    super(message, 500);
-  }
-}
-
-module.exports = InternalServerError;
