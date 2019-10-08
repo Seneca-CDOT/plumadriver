cae960f921496353aa3aed661ca836ef8f81f7f1
Pouya Oftadeh <poftadeh2@gmail.com>
2019-10-07 20:12:54 -0400

    fix: remove extra character from error message

--- src/Error/ElementNotInteractable.ts
+++ src/Error/ElementNotInteractable.ts
@@ -4,7 +4,7 @@ export class ElementNotInteractable extends BadRequest {
   constructor() {
     super();
     this.name = 'ElementNotInteractableError';
-    this.message = `${this.command} could not be completed because the element is not pointer- or keyboard interactable.`;
+    this.message = `${this.command} could not be completed because the element is not pointer or keyboard interactable.`;
     this.JSONCodeError = 'element not interactable';
   }
 }
