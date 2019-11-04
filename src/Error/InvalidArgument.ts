880609461a353535cd8b8e0ec1345e25b8e5ee90
Pouya Oftadeh <poftadeh2@gmail.com>
2019-11-04 16:36:56 -0500

    fix: invalid argument error message formatting

--- src/Error/InvalidArgument.ts
+++ src/Error/InvalidArgument.ts
@@ -3,7 +3,9 @@ import { BadRequest } from './BadRequest';
 export class InvalidArgument extends BadRequest {
   constructor(reason?: string) {
     super();
-    this.message = `The arguments passed to this command are either invalid or malformed: ${reason}`;
+    this.message = `The arguments passed to this command are either invalid or malformed${
+      reason ? ` :${reason}` : '.'
+    }`;
     this.name = 'InvalidArgumentError';
     this.JSONCodeError = 'invalid argument';
   }
