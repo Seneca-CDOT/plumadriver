3cf103120fe439008eff7f0d63f68356fa279848
Pouya Oftadeh <poftadeh2@gmail.com>
2019-11-06 18:58:37 -0500

    feat: add utils tests

---
+++ test/jest/utils/utils.test.js
@@ -0,0 +1,13 @@
+const { extractDomainFromString } = require('../../../build/utils/utils');
+
+describe('Utils Functions', () => {
+  it('extractDomainFromString', () => {
+    expect(extractDomainFromString('http://www.foo.com')).toEqual('foo.com');
+    expect(extractDomainFromString('.foo.com')).toEqual('foo.com');
+    expect(extractDomainFromString('https://foo.bar.local/foo/ui/portal')).toEqual('bar.local');
+    expect(extractDomainFromString('.bar.local')).toEqual('bar.local');
+    expect(extractDomainFromString('http://foo.example.com/path/to/page?name=ferret&color=purple')).toEqual('example.com');
+    expect(extractDomainFromString('ftp://127.0.0.1:5500/index.html#foo?bar=baz')).toEqual('127.0.0.1');
+    expect(extractDomainFromString('https://www.gojobs.gov.on.ca/Jobs.aspx')).toEqual('gov.on.ca');
+  })
+})
