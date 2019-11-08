0b660f06dd86ccc4dd1af0d2c274dca699d797b9
Pouya Oftadeh <poftadeh2@gmail.com>
2019-11-08 12:53:54 -0500

    feat: add cookie validator function tests

--- test/jest/Browser/CookieValidator.test.js
+++ test/jest/Browser/CookieValidator.test.js
@@ -1,16 +1,84 @@
 const { CookieValidator } = require('../../../build/Browser/CookieValidator');
 
 describe('CookieValidator Class', () => {
-  it('correctly validates domains', () => {
-    expect(CookieValidator.isValidDomain('foo.com', 'foo.com')).toBe(true);
-    expect(CookieValidator.isValidDomain('.foo.com', 'foo.com')).toBe(true);
-    expect(CookieValidator.isValidDomain('.foo.ca', 'foo.com')).toBe(false);
-    expect(CookieValidator.isValidDomain('.foo', 'foo.com')).toBe(false);
-    expect(CookieValidator.isValidDomain('.foo.com.', 'foo.com')).toBe(false);
-    expect(CookieValidator.isValidDomain('gov.on.ca', 'on.ca')).toBe(false);
-    expect(CookieValidator.isValidDomain('.', 'foo.com')).toBe(false);
-    expect(CookieValidator.isValidDomain('.foo.com/foo', 'foo.com')).toBe(
+  it('validates names'),
+    () => {
+      expect(CookieValidator.isValidName('name')).toBe(true);
+      expect(CookieValidator.isValidName(false)).toBe(false);
+      expect(CookieValidator.isValidName('')).toBe(false);
+    };
+
+  it('validates values'),
+    () => {
+      expect(CookieValidator.isValidValue('foo')).toBe(true);
+      expect(CookieValidator.isValidValue(true)).toBe(false);
+      expect(CookieValidator.isValidValue('')).toBe(false);
+    };
+
+  it('validates domains', () => {
+    expect(CookieValidator.isValidDomain('google.com', 'google.com')).toBe(
+      true,
+    );
+    expect(CookieValidator.isValidDomain('.google.com', 'google.com')).toBe(
       false,
     );
+    expect(CookieValidator.isValidDomain('foo.co.uk', 'co.uk')).toBe(false);
+  });
+
+  it('validates expiry', () => {
+    expect(CookieValidator.isValidExpiry(1573232663883)).toBe(true);
+    expect(CookieValidator.isValidExpiry(new Date())).toBe(false);
+    expect(CookieValidator.isValidExpiry(Number.MAX_SAFE_INTEGER + 1)).toBe(
+      false,
+    );
+    expect(CookieValidator.isValidExpiry(-1573232663883)).toBe(false);
+  });
+
+  it('validates httpOnly', () => {
+    expect(CookieValidator.isValidHttpOnly(true)).toBe(true);
+    expect(CookieValidator.isValidHttpOnly('false')).toBe(false);
+  });
+
+  it('validates secure', () => {
+    expect(CookieValidator.isValidHttpOnly(false)).toBe(true);
+    expect(CookieValidator.isValidHttpOnly('true')).toBe(false);
+  });
+
+  it('validates cookies', () => {
+    expect(
+      CookieValidator.isValidCookie({
+        name: 'foo',
+        value: 'bar',
+        httpOnly: true,
+        secure: false,
+        expiry: 1573232663883,
+        domain: '.google.com',
+      }),
+      'google.com',
+    ).toBe(true);
+
+    expect(
+      CookieValidator.isValidCookie({
+        name: 'foo',
+        value: 'bar',
+      }),
+      'site.co.uk',
+    ).toBe(true);
+
+    expect(
+      CookieValidator.isValidCookie({
+        name: 'foo',
+      }),
+      'site.co.uk',
+    ).toBe(false);
+
+    expect(
+      CookieValidator.isValidCookie({
+        name: 'foo',
+        value: 'bar',
+        domain: 'co.uk',
+      }),
+      'site.co.uk',
+    ).toBe(false);
   });
 });
