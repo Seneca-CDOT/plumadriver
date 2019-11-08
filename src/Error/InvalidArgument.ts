217bdb99c6de364f2c904a5ca26c3b381432278d
Pouya Oftadeh <poftadeh2@gmail.com>
2019-11-08 16:27:34 -0500

    fix: error messages in UnableToSetCookie and InvalidArgument

--- src/Error/InvalidArgument.ts
+++ src/Error/InvalidArgument.ts
@@ -3,9 +3,9 @@ import { BadRequest } from './BadRequest';
 export class InvalidArgument extends BadRequest {
   constructor(reason?: string) {
     super();
-    this.message = `The arguments passed to this command are either invalid or malformed${
-      reason ? ` :${reason}` : '.'
-    }`;
+    this.message =
+      reason ||
+      'The arguments passed to this command are either invalid or malformed.';
     this.name = 'InvalidArgumentError';
     this.JSONCodeError = 'invalid argument';
   }
--- src/Error/UnableToSetCookie.ts
+++ src/Error/UnableToSetCookie.ts
@@ -2,11 +2,7 @@ import { InternalServerError } from './InternalServerError';
 
 export class UnableToSetCookie extends InternalServerError {
   constructor(reason?: string) {
-    super(
-      `A command to set a cookie’s value could not be satisfied${
-        reason ? ` :${reason}` : '.'
-      }`,
-    );
+    super(reason || 'A command to set a cookie’s value could not be satisfied');
 
     this.name = 'UnableToSetCookie';
     this.JSONCodeError = 'unable to set cookie';
