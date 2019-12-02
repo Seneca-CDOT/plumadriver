eaed74541a293901669aab64d293102d32905069
Pouya Oftadeh <poftadeh2@gmail.com>
2019-12-02 13:13:09 -0500

    feat: add helper file for tests

---
+++ test/jest/e2e/helpers.js
@@ -0,0 +1,29 @@
+
+exports.createSession = async (request, app) => {
+  const requestBody = {
+    desiredCapabilities: {
+      browserName: 'pluma',
+      unhandledPromptBehavior: 'ignore',
+      'plm:plumaOptions': { runScripts: true },
+    },
+    capabilities: {
+      firstMatch: [
+        {
+          browserName: 'pluma',
+          'plm:plumaOptions': { runScripts: true },
+          unhandledPromptBehavior: 'ignore',
+        },
+      ],
+    },
+  };
+  
+  const {
+    body: {
+      value: { sessionId },
+    },
+  } = await request(app)
+    .post('/session')
+    .send(requestBody);
+
+  return sessionId;
+};
