573965369c777845576c24aa441218c8cc9b2abe
Pouya Oftadeh <poftadeh2@gmail.com>
2019-12-03 17:52:57 -0500

    test: fix nock configuration in execute script test

--- test/jest/e2e/execute-script-sync.test.js
+++ test/jest/e2e/execute-script-sync.test.js
@@ -6,6 +6,8 @@ const { createSession } = require('./helpers');
 const { ELEMENT } = require('../../../build/constants/constants');
 
 describe('Execute Script Sync', () => {
+  let sessionId;
+
   beforeAll(async () => {
     nock(/plumadriver\.com/)
       .get('/')
@@ -16,6 +18,7 @@ describe('Execute Script Sync', () => {
           <title>Test Page</title>
         </head>
         <body>
+          <input type="text" value='baz' />
           <p id="foo">bar</p>
           <button onclick="clickAction(this)">
             not clicked
@@ -26,8 +29,8 @@ describe('Execute Script Sync', () => {
             }
           </script>
           <script>
-            function getTextContent() {
-              return document.querySelector('title').textContent;
+            function getInputValue() {
+              return document.querySelector('input').value;
             }
 
             function getParagraphElement() {
@@ -37,9 +40,16 @@ describe('Execute Script Sync', () => {
         </body>
       </html>`,
       );
+
+    sessionId = await createSession(request, app);
+    await request(app)
+      .post(`/session/${sessionId}/url`)
+      .send({
+        url: 'http://plumadriver.com',
+      });
   });
 
-  const executeScript = async (sessionId, script, args = []) => {
+  const executeScript = async (script, args = []) => {
     const {
       body: { value },
     } = await request(app)
@@ -50,37 +60,28 @@ describe('Execute Script Sync', () => {
   };
 
   it('handles truthy values', async () => {
-    const sessionId = await createSession(request, app);
-
-    expect(await executeScript(sessionId, 'return true')).toBe(true);
-    expect(await executeScript(sessionId, 'return 1')).toBe(1);
-    expect(await executeScript(sessionId, 'return "false"')).toBe('false');
-    expect(await executeScript(sessionId, 'return "0"')).toBe('0');
+    expect(await executeScript('return true')).toBe(true);
+    expect(await executeScript('return 1')).toBe(1);
+    expect(await executeScript('return "false"')).toBe('false');
+    expect(await executeScript('return "0"')).toBe('0');
     expect(
-      await executeScript(sessionId, sessionId, 'return arguments', [
-        { foo: 'bar' },
-      ]),
-    ).toEqual([{ foo: 'bar' }]);
+      await executeScript('return arguments[0]', [{ foo: 'bar' }]),
+    ).toStrictEqual({ foo: 'bar' });
   });
 
   it('handles falsy values', async () => {
-    const sessionId = await createSession(request, app);
-
-    expect(await executeScript(sessionId, 'return false')).toBe(false);
-    expect(await executeScript(sessionId, 'return NaN')).toBe(NaN);
-    expect(await executeScript(sessionId, 'return 0')).toBe(0);
-    expect(await executeScript(sessionId, 'return !!document.all')).toBe(false);
-    expect(
-      await executeScript(sessionId, 'return Boolean(...arguments[0])', [['']]),
-    ).toBe(false);
+    expect(await executeScript('return false')).toBe(false);
+    expect(await executeScript('return NaN')).toBe(NaN);
+    expect(await executeScript('return 0')).toBe(0);
+    expect(await executeScript('return !!document.all')).toBe(false);
+    expect(await executeScript('return Boolean(...arguments[0])', [['']])).toBe(
+      false,
+    );
   });
 
   it('handles Promises', async () => {
-    const sessionId = await createSession(request, app);
-
     expect(
       await executeScript(
-        sessionId,
         `return new Promise((resolve) => setTimeout(() => resolve(arguments[0] + ' ' + arguments[1]), 400))`,
         ['hello', 'world!'],
       ),
@@ -88,71 +89,53 @@ describe('Execute Script Sync', () => {
   });
 
   it('sums two arguments', async () => {
-    const sessionId = await createSession(request, app);
-
     expect(
-      await executeScript(sessionId, 'return arguments[0] + arguments[1];', [
-        1,
-        2,
-      ]),
+      await executeScript('return arguments[0] + arguments[1];', [1, 2]),
     ).toBe(3);
   });
 
   it('returns the value of document.title', async () => {
-    const sessionId = await createSession(request, app);
-
     expect(
-      await executeScript(sessionId, 'return document[arguments[0]];', [
-        'title',
-      ]),
+      await executeScript('return document[arguments[0]];', ['title']),
     ).toBe('Test Page');
   });
 
   it('recognizes window and querySelector', async () => {
-    const sessionId = await createSession(request, app);
-
     expect(
       await executeScript(
-        sessionId,
         'return window.document.querySelector("#foo").textContent;',
       ),
     ).toBe('bar');
   });
 
   it('throws a JavaScriptError on an invalid script body', async () => {
-    const sessionId = await createSession(request, app);
-    const { error } = await executeScript(sessionId, 'return null.foo');
-
+    const { error } = await executeScript('return null.foo');
     expect(error).toBe('javascript error');
   });
 
   it('throws ScriptTimeout when execution takes longer than the set limit', async () => {
-    const sessionId = await createSession(request, app);
-
     await request(app)
       .post(`/session/${sessionId}/timeouts`)
       .send({
         script: 50,
       });
 
-    const { error } = await executeScript(sessionId, 'while (true) {}');
+    const { error } = await executeScript('while (true) {}');
     expect(error).toBe('script timeout');
   });
 
   it('returns an array of HTMLElements', async () => {
-    const sessionId = await createSession(request, app);
-
     expect(
       await executeScript(
-        sessionId,
         'return [document.querySelector("p"), document.querySelector("button")]',
       ),
-    ).toBe(false);
+    ).toStrictEqual([
+      { [ELEMENT]: expect.any(String) },
+      { [ELEMENT]: expect.any(String) },
+    ]);
   });
 
   it('clicks on a found element', async () => {
-    const sessionId = await createSession(request, app);
-
     const {
       body: { value },
     } = await request(app)
@@ -161,34 +144,18 @@ describe('Execute Script Sync', () => {
 
     expect(
       await executeScript(
-        sessionId,
         'arguments[0].click(); return arguments[0].textContent',
         [{ [ELEMENT]: value[ELEMENT] }],
       ),
     ).toBe('click success');
   });
 
-  // it('handles global functions', async () => {
-  //   const sessionId = await createSession(request, app);
-
-  //   expect(await executeScript(sessionId, 'return false')).toBe(false);
-
-  //   const textContent = await session.process({
-  //     command: COMMANDS.EXECUTE_SCRIPT,
-  //     parameters: {
-  //       script: 'return getTextContent();',
-  //       args: [],
-  //     },
-  //   });
-  //   expect(textContent).toBe('Test Page');
-
-  //   const paragraphElement = await session.process({
-  //     command: COMMANDS.EXECUTE_SCRIPT,
-  //     parameters: {
-  //       script: 'return getParagraphElement();',
-  //       args: [],
-  //     },
-  //   });
-  //   expect(paragraphElement).toHaveProperty([ELEMENT], expect.any(String));
-  // });
+  it('handles global functions', async () => {
+    expect(await executeScript('return getInputValue()')).toBe(
+      'baz',
+    );
+    expect(
+      await executeScript('return getParagraphElement();'),
+    ).toHaveProperty([ELEMENT], expect.any(String));
+  });
 });
