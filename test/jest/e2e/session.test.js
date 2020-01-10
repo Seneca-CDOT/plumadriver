a999e02e716cc3fef9e1c2ed195f609a268eb867
Pouya Oftadeh <poftadeh2@gmail.com>
2020-01-10 14:45:17 -0500

    test: add alwaysMatch capabilities test

--- test/jest/e2e/session.test.js
+++ test/jest/e2e/session.test.js
@@ -1,8 +1,57 @@
 const request = require('supertest');
+const nock = require('nock');
 
 const { app } = require('../../../build/app');
 
 describe('Session', () => {
+  const createSession = async requestBody => {
+    const { body } = await request(app)
+      .post('/session')
+      .send(requestBody);
 
-  
+    return body;
+  };
+
+  it('creates a session with alwaysMatch capabilities', async () => {
+    const requestBody = {
+      capabilities: {
+        alwaysMatch: {
+          acceptInsecureCerts: false,
+          browserName: 'pluma',
+          pageLoadStrategy: 'normal',
+          unhandledPromptBehavior: 'ignore',
+          timeouts: {
+            implicit: 100,
+            pageLoad: 200,
+            script: 300,
+          },
+          'plm:plumaOptions': { runScripts: true },
+        },
+      },
+    };
+
+    expect(await createSession(requestBody)).toMatchObject({
+      value: {
+        capabilities: {
+          acceptInsecureCerts: true,
+          browserName: 'pluma',
+          browserVersion: expect.any(String),
+          pageLoadStrategy: 'normal',
+          platformName: expect.any(String),
+          unhandledPromptBehaviour: 'ignore',
+          timeouts: {
+            implicit: 100,
+            pageLoad: 200,
+            script: 300,
+          },
+          proxy: {},
+          'plm:plumaOptions': { runScripts: 'dangerously' },
+          setWindowRect: expect.any(Boolean),
+        },
+        sessionId: expect.any(String),
+      },
+    });
+  });
+
+  it('finds matching capabilities', async () => {});
 });
