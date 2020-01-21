28b3d7b43c39c4d6f52d41dedda91c6fe411e5b1
Pouya Oftadeh <poftadeh2@gmail.com>
2020-01-21 11:22:09 -0500

    test: add InvalidArgument cookie check

--- test/jest/e2e/cookies.test.js
+++ test/jest/e2e/cookies.test.js
@@ -8,20 +8,20 @@ describe('Cookies', () => {
   let sessionId;
 
   beforeAll(async () => {
-    nock(/plumadriver\.com/)
+    nock(/plumadriver/)
       .get('/')
       .reply(200, '<html></html>');
 
     sessionId = await createSession(request, app);
   });
 
-  const addCookie = async cookie => {
+  const addCookie = async (cookie, expectedStatusCode = 200) => {
     const {
       body: { value },
     } = await request(app)
       .post(`/session/${sessionId}/cookie`)
       .send({ cookie })
-      .expect(200);
+      .expect(expectedStatusCode);
 
     return value;
   };
@@ -32,7 +32,8 @@ describe('Cookies', () => {
     } = await request(app)
       .get(`/session/${sessionId}/cookie/${name}`)
       .expect(200);
-    console.log('doy', value);
+
+    console.log(value);
     return value;
   };
 
@@ -43,7 +44,7 @@ describe('Cookies', () => {
         url: 'http://plumadriver.com',
       });
 
-    const cookie = {
+    const requestCookie = {
       secure: false,
       httpOnly: false,
       expiry: 3654907200,
@@ -53,10 +54,88 @@ describe('Cookies', () => {
       value: 'isValid',
     };
 
-    await addCookie(cookie);
-    expect(await getNamedCookie(cookie.name)).toMatchObject({
-      name: cookie.name,
-      value: cookie.value,
+    await addCookie(requestCookie);
+    expect(await getNamedCookie(requestCookie.name)).toMatchObject({
+      name: requestCookie.name,
+      value: requestCookie.value,
+      domain: requestCookie.domain,
+      path: requestCookie.path,
     });
+  });
+
+  it('handles dot prefix in cookie domains', async () => {
+    await request(app)
+      .post(`/session/${sessionId}/url`)
+      .send({
+        url: 'http://plumadriver.com/',
+      });
+
+    const requestCookie = {
+      secure: true,
+      httpOnly: true,
+      expiry: 1573253325754,
+      domain: '.plumadriver.com',
+      name: 'foo',
+      path: '/',
+      value: 'bar',
+    };
+
+    await addCookie(requestCookie);
+    expect(await getNamedCookie(requestCookie.name)).toMatchObject({
+      name: requestCookie.name,
+      value: requestCookie.value,
+      domain: 'plumadriver.com',
+    });
+  });
+
+  it('adds a cookie filling in missing optional fields', async () => {
+    await request(app)
+      .post(`/session/${sessionId}/url`)
+      .send({
+        url: 'http://www.plumadriver.com/',
+      });
+
+    const requestCookie = {
+      name: 'foo',
+      value: 'bar',
+    };
+
+    await addCookie(requestCookie);
+    expect(await getNamedCookie(requestCookie.name)).toMatchObject({
+      name: requestCookie.name,
+      value: requestCookie.value,
+      domain: 'plumadriver.com',
+      path: '/',
+    });
+  });
+
+  it('throws InvalidArgument error on invalid fields', async () => {
+    await request(app)
+      .post(`/session/${sessionId}/url`)
+      .send({
+        url: 'http://plumadriver.com/',
+      });
+
+    await addCookie({ value: 'foo' });
+
+    await addCookie({
+      name: 'foo',
+      value: 'bar',
+      expiry: -1,
+    }, 400);
+
+    await addCookie(
+      addCookieAndAssertError(browser, {
+        name: 'foo',
+        value: 'bar',
+        httpOnly: 'true',
+      }, 400),
+    );
+
+    await addCookie({
+      name: 'foo',
+      value: 'bar',
+      secure: 'false',
+    }, 400);
   });
 });
